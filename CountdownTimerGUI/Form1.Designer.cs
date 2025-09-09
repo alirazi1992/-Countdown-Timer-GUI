b7f5bd40c923f5a5a93af3b8b8a02bfadc378b7b
Ali Razi <95105244+alirazi1992@users.noreply.github.com>
2025-09-09 11:02:07 +0330

    Add files via upload

---
+++ CountdownTimerGUI/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace CountdownTimerGUI
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.TextBox txtMinutes;
+        private System.Windows.Forms.TextBox txtSeconds;
+        private System.Windows.Forms.Label lblMin;
+        private System.Windows.Forms.Label lblSec;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Timer timer;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.txtMinutes = new System.Windows.Forms.TextBox();
+            this.txtSeconds = new System.Windows.Forms.TextBox();
+            this.lblMin = new System.Windows.Forms.Label();
+            this.lblSec = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Font = new System.Drawing.Font("Consolas", 28F, System.Drawing.FontStyle.Bold);
+            this.lblTimer.Location = new System.Drawing.Point(30, 20);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(120, 45);
+            this.lblTimer.TabIndex = 0;
+            this.lblTimer.Text = "00:00";
+            // 
+            // txtMinutes
+            // 
+            this.txtMinutes.Location = new System.Drawing.Point(30, 80);
+            this.txtMinutes.Name = "txtMinutes";
+            this.txtMinutes.Size = new System.Drawing.Size(50, 20);
+            this.txtMinutes.TabIndex = 1;
+            // 
+            // txtSeconds
+            // 
+            this.txtSeconds.Location = new System.Drawing.Point(110, 80);
+            this.txtSeconds.Name = "txtSeconds";
+            this.txtSeconds.Size = new System.Drawing.Size(50, 20);
+            this.txtSeconds.TabIndex = 2;
+            // 
+            // lblMin
+            // 
+            this.lblMin.AutoSize = true;
+            this.lblMin.Location = new System.Drawing.Point(30, 105);
+            this.lblMin.Name = "lblMin";
+            this.lblMin.Size = new System.Drawing.Size(44, 13);
+            this.lblMin.TabIndex = 3;
+            this.lblMin.Text = "Minutes";
+            // 
+            // lblSec
+            // 
+            this.lblSec.AutoSize = true;
+            this.lblSec.Location = new System.Drawing.Point(110, 105);
+            this.lblSec.Name = "lblSec";
+            this.lblSec.Size = new System.Drawing.Size(49, 13);
+            this.lblSec.TabIndex = 4;
+            this.lblSec.Text = "Seconds";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(30, 140);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(60, 30);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(100, 140);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(60, 30);
+            this.btnStop.TabIndex = 6;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(170, 140);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(60, 30);
+            this.btnReset.TabIndex = 7;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(260, 200);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblSec);
+            this.Controls.Add(this.lblMin);
+            this.Controls.Add(this.txtSeconds);
+            this.Controls.Add(this.txtMinutes);
+            this.Controls.Add(this.lblTimer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Countdown Timer (Day 16)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ CountdownTimerGUI/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CountdownTimerGUI
+{
+    public partial class Form1 : Form
+    {
+        private int _timeLeft; // in seconds
+
+        public Form1()
+        {
+            InitializeComponent();
+            lblTimer.Text = "00:00";
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            int minutes, seconds;
+
+            if (!int.TryParse(txtMinutes.Text, out minutes)) minutes = 0;
+            if (!int.TryParse(txtSeconds.Text, out seconds)) seconds = 0;
+
+            _timeLeft = minutes * 60 + seconds;
+
+            if (_timeLeft <= 0)
+            {
+                MessageBox.Show("Please enter a valid time.", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            timer.Interval = 1000;
+            timer.Start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            _timeLeft = 0;
+            lblTimer.Text = "00:00";
+            txtMinutes.Clear();
+            txtSeconds.Clear();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (_timeLeft > 0)
+            {
+                _timeLeft--;
+                int minutes = _timeLeft / 60;
+                int seconds = _timeLeft % 60;
+                lblTimer.Text = string.Format("{0:00}:{1:00}", minutes, seconds);
+            }
+            else
+            {
+                timer.Stop();
+                lblTimer.Text = "00:00";
+                System.Media.SystemSounds.Exclamation.Play();
+                MessageBox.Show("⏰ Time’s up!", "Countdown Finished",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
